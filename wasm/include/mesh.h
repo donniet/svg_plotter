a204cac6425839b5418728282f92f5a91806b798
Donnie Tolbert <donniet@gmail.com>
2025-03-26 13:47:48 -0500

    the javascript is in good shape, pivoting to the C exporter for WASM

--- wasm/include/mesh.h
+++ wasm/include/mesh.h
@@ -1,6 +1,8 @@
 #ifndef __MESH_H__
 #define __MESH_H__
 
+typedef unsigned int uint;
+
 typedef enum {
     BrushRound = 0
 } BrushStyle;
@@ -21,37 +23,33 @@ typedef enum {
 
 typedef struct
 {
-    unsigned int start, end;
-} VertexRange;
+    uint start, end;
+} IndexRange;
 
 typedef struct
 {
-    unsigned int start, end;
-} SectionRange;
-
-typedef struct
-{
+    const char * name;
     BrushStyle brush_style;
     float brush_size;
     RGBA brush_color;
     TimeRange time_range;
     DrawMode draw_mode;
     float arclength;
-    VertexRange vertex_range;
-    SectionRange section_range;
+    IndexRange vertex_range;
+    IndexRange section_range;
 } Stroke;
 
 typedef struct
 {
     const char * name;
-    unsigned int size;
-    unsigned int offset;
+    uint size;
+    uint offset;
 } Attribute;
 
 typedef struct
 {
     const char * name;
-    unsigned int size;
+    uint size;
     const float * data;
 } Uniform;
 
@@ -61,146 +59,216 @@ typedef struct
  * These will be filled in by the output of stroke_mesh
  */
 Stroke const _strokes[];
-unsigned int _stroke_count;
+uint _stroke_count;
 Attribute const _attributes[];
-unsigned int _attribute_count;
+uint _attribute_count;
 Uniform const _uniforms[];
-unsigned int _uniform_count;
+uint _uniform_count;
 float const _buffer[];
-unsigned int _buffer_size;
+uint _buffer_size;
+uint _stride;
 
-unsigned int string_length(const char * s)
+uint _section_end_vertex[];
+float _section_cumulative_arclength[];
+
+float _drawing_width;
+float _drawing_height;
+
+float _draw_time;
+float _clear_color[4];
+
+uint string_length(const char * s)
 {
-    unsigned int l = 0;
+    uint l = 0;
     while(s[l] != '\0')
         l++;
 
     return l;
 }
 
-extern unsigned int stroke_count()
+extern float drawing_width()
+{
+    return _drawing_width;
+}
+
+extern float drawing_height()
+{
+    return _drawing_height;
+}
+
+extern float draw_time()
+{
+    return _draw_time;
+}
+
+extern const float * clear_color_ptr()
+{
+    return _clear_color;
+}
+extern uint clear_color_length()
+{
+    return 4;
+}
+
+extern uint stroke_count()
 {
     return _stroke_count;
 }
 
-extern BrushStyle get_stroke_brush_style(unsigned int i)
+extern const char * stroke_name_ptr(uint i)
 {
-    return _strokes[i].brush_style;
+    return _strokes[i].name;
 }
 
-extern float get_stroke_brush_size(unsigned int i)
+extern uint stroke_name_length(uint i)
 {
-    return _strokes[i].brush_size;
+    return string_length(_strokes[i].name);
 }
 
-extern float get_stroke_brush_color_r(unsigned int i)
-{
-    return _strokes[i].brush_color.r;
-}
-extern float get_stroke_brush_color_g(unsigned int i)
-{
-    return _strokes[i].brush_color.g;
-}
-extern float get_stroke_brush_color_b(unsigned int i)
-{
-    return _strokes[i].brush_color.b;
-}
-extern float get_stroke_brush_color_a(unsigned int i)
-{
-    return _strokes[i].brush_color.a;
-}
-
-extern float get_stroke_time_start(unsigned int i)
-{
-    return _strokes[i].time_range.start;
-}
-extern float get_stroke_time_end(unsigned int i)
-{
-    return _strokes[i].time_range.end;
-}
-
-extern DrawMode get_stroke_draw_mode(unsigned int i)
-{
-    return _strokes[i].draw_mode;
-}
-
-extern float get_stroke_arclength(unsigned int i)
-{
-    return _strokes[i].arclength;
-}
-
-extern unsigned int get_stroke_vertex_start(unsigned int i)
+extern uint stroke_vertex_start(uint i)
 {
     return _strokes[i].vertex_range.start;
 }
-extern unsigned int get_stroke_vertex_end(unsigned int i)
+
+extern uint stroke_vertex_end(uint i)
 {
     return _strokes[i].vertex_range.end;
 }
 
-extern unsigned int get_stroke_section_start(unsigned int i)
+extern uint stroke_section_count(uint i)
 {
-    return _strokes[i].section_range.start;
-}
-extern unsigned int get_stroke_section_end(unsigned int i)
-{
-    return _strokes[i].section_range.end;
+    return _strokes[i].section_range.end - _strokes[i].section_range.start;
 }
 
+extern BrushStyle stroke_brush_style(uint i)
+{
+    return _strokes[i].brush_style;
+}
 
-extern unsigned int attribute_count()
+extern float stroke_brush_size(uint i)
+{
+    return _strokes[i].brush_size;
+}
+
+extern const float * stroke_brush_color_ptr(uint i)
+{
+    return &_strokes[i].brush_color.r;
+}
+extern uint stroke_brush_color_length(uint i)
+{
+    return 4;
+}
+
+extern float stroke_brush_color_r(uint i)
+{
+    return _strokes[i].brush_color.r;
+}
+extern float stroke_brush_color_g(uint i)
+{
+    return _strokes[i].brush_color.g;
+}
+extern float stroke_brush_color_b(uint i)
+{
+    return _strokes[i].brush_color.b;
+}
+extern float stroke_brush_color_a(uint i)
+{
+    return _strokes[i].brush_color.a;
+}
+
+extern float stroke_time_start(uint i)
+{
+    return _strokes[i].time_range.start;
+}
+extern float stroke_time_end(uint i)
+{
+    return _strokes[i].time_range.end;
+}
+
+extern DrawMode stroke_draw_mode(uint i)
+{
+    return _strokes[i].draw_mode;
+}
+
+extern float stroke_arclength(uint i)
+{
+    return _strokes[i].arclength;
+}
+
+extern uint stroke_section_end_vertex(
+    uint stroke_index, uint section_index)
+{
+    return _section_end_vertex[
+        _strokes[stroke_index].section_range.start + section_index
+    ];
+}
+
+extern float stroke_section_cumulative_arclength(
+    uint stroke_index, uint section_index)
+{
+    return _section_cumulative_arclength[
+        _strokes[stroke_index].section_range.start + section_index
+    ];
+}
+
+extern uint attribute_count()
 {
     return _attribute_count;
 }
-extern const char * get_attribute_name_ptr(unsigned int i)
+extern const char * attribute_name_ptr(uint i)
 {
     return _attributes[i].name;
 }
-extern unsigned int get_attribute_name_length(unsigned int i)
+extern uint attribute_name_length(uint i)
 {
-    const char * s = get_attribute_name_ptr(i);
+    const char * s = attribute_name_ptr(i);
     
     return string_length(s);
 }
-extern unsigned int get_attribute_size(unsigned int i)
+extern uint attribute_size(uint i)
 {
     return _attributes[i].size;
 }
-extern unsigned int get_attribute_offset(unsigned int i)
+extern uint attribute_offset(uint i)
 {
     return _attributes[i].offset;
 }
 
-extern unsigned int uniform_count()
+extern uint uniform_count()
 {
     return _uniform_count;
 }
 
-extern const char * get_uniform_name_ptr(unsigned int i)
+extern const char * uniform_name_ptr(uint i)
 {
     return _uniforms[i].name;
 }
-extern unsigned int get_uniform_name_length(unsigned int i)
+extern uint uniform_name_length(uint i)
 {
-    const char * s = get_uniform_name_ptr(i);
+    const char * s = uniform_name_ptr(i);
     
     return string_length(s);
 }
 
-extern unsigned int get_uniform_size(unsigned int i)
+extern uint uniform_size(uint i)
 {
     return _uniforms[i].size;
 }
-extern const float * get_uniform_data(unsigned int i)
+extern const float * uniform_data(uint i)
 {
     return _uniforms[i].data;
 }
 
-extern unsigned int buffer_size()
+extern uint stride()
+{
+    return _stride;
+}
+
+extern uint buffer_size()
 {
     return _buffer_size;
 }
-extern float const * get_buffer()
+extern float const * buffer()
 {
     return _buffer;
 }
